cc0f32231eee7720e01218d5c4b058de6034d71c
Ganesh Bhatta <bhattaganesh205@gmail.com>
2021-10-26 15:30:58 +0545

    trying to solve 500 server error

--- pfms/settings.py
+++ pfms/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY = 'django-insecure-kq$tce^l&-6_5_ifua@1+m2y7rvf72-4s^9(i&ywv*exg^5ht2'
 SECRET_KEY =  os.environ.get('SECRET_KEY'),
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 # ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'new-pfms.herokuapp.com']
