b3736a9fc6fc67c255970808ed3ef2662fc144af
Ganesh Bhatta <bhattaganesh205@gmail.com>
2021-10-31 22:51:57 +0545

    admin panel customization

--- expense/admin.py
+++ expense/admin.py
@@ -2,4 +2,33 @@ from django.contrib import admin
 from .models import Expense, ExpenseCategory
 
 # Register your models here.
-admin.site.register([ExpenseCategory, Expense])
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = (
+        'amount',
+        'description',
+        'expense_by',
+        'expense_category',
+        'expense_date',
+        'entry_date',
+        'updated_at',
+    )
+
+    search_fields = (
+        'amount',
+        'description',
+        'expense_by',
+        'expense_category',
+        'expense_date',
+        'entry_date',
+        'updated_at',
+    )
+    list_per_page = 5
+
+class ExpenseCategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description')
+    search_fields = ('title', 'description')
+    list_per_page = 5
+
+
+admin.site.register(Expense, ExpenseAdmin)
+admin.site.register(ExpenseCategory, ExpenseCategoryAdmin)
--- income/admin.py
+++ income/admin.py
@@ -3,4 +3,34 @@ from django.contrib import admin
 from income.models import Income, IncomeCategory
 
 # Register your models here.
-admin.site.register([Income, IncomeCategory])
+class IncomeAdmin(admin.ModelAdmin):
+    list_display = (
+        'amount',
+        'description',
+        'income_by',
+        'income_category',
+        'income_date',
+        'entry_date',
+        'updated_at',
+    )
+
+    search_fields = (
+        'amount',
+        'description',
+        'income_by',
+        'income_category',
+        'income_date',
+        'entry_date',
+        'updated_at',
+    )
+
+    list_per_page = 5
+class IncomeCategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description')
+    search_fields = ('title', 'description')
+    list_per_page = 5
+
+
+
+admin.site.register(Income, IncomeAdmin)
+admin.site.register(IncomeCategory, IncomeCategoryAdmin)
